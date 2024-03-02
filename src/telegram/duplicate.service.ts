5e92340229b5cccecda41950cf6a433e2605c871
pawelmat142 <pawelmat142@gmail.com>
2024-03-02 17:30:52 +0100

    cleaning too many forward messages collection

--- src/telegram/duplicate.service.ts
+++ src/telegram/duplicate.service.ts
@@ -1,15 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { Model } from "mongoose";
 import { ForwardMessage } from "./forward-message";
 import { InjectModel } from "@nestjs/mongoose";
 import { TelegramMessage } from "./message";
+import { Cron, CronExpression } from "@nestjs/schedule";
 
 @Injectable()
 export class DuplicateService {
 
+    private readonly logger = new Logger(DuplicateService.name)
+
     constructor(
         @InjectModel(ForwardMessage.name) private forwardMessageModel: Model<ForwardMessage>,
     ) {}
+
+    @Cron(CronExpression.EVERY_DAY_AT_3AM)
+    private async cleanForwardMessages() {
+        const latest50Documents = await this.forwardMessageModel
+            .find({}, { _id: true })
+            .sort({ _id: -1 })
+            .limit(50)
+            .exec()
+
+        const latest50Ids = latest50Documents.map(d => d._id)
+        const res = await this.forwardMessageModel.deleteMany({ _id: { $nin: latest50Ids } }).exec()
+        this.logger.log(`Cleaned up ${res?.deletedCount} messages `)
+    }
+
 
     public async saveMessage(message: TelegramMessage) {
         const updateChannelId = message.peer_id?.channel_id
